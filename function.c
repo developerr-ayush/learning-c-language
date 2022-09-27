e128ed364c506b35b4cc3c5aa95386ef6be3d209
shahayush480 <79455386+shahayush480@users.noreply.github.com>
2022-09-27 19:33:42 +0530

    .

---
+++ function.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+// function to print factorial of n
+void number(int* n);
+int main()
+{
+    int n = 10;
+    printf("%d \n",n);
+
+    // number();
+
+    printf("%d \n",);
+    return 0;
+}
+
+void number(int* n)
+{
+    *n = *n * *n;
+}
